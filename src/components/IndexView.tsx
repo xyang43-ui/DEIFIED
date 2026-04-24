4f1ecdff1d3eb68f8e43fb33fca5c042bf8855b5
xyang43-ui <xyang43@pratt.edu>
2026-04-24 05:38:02 -0400

    update mobile view

--- src/components/IndexView.tsx
+++ src/components/IndexView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrambledText, TypewriterText } from './Common';
 
 interface IndexViewProps {
